0be0763bd4d821b3a2b1b12d248fe21f304c5c9d
José Suárez <josegsuarezg@gmail.com>
2022-05-09 12:48:08 -0300

    Agregando Enlaces a Cursos

--- components/Curso.js
+++ components/Curso.js
@@ -2,19 +2,21 @@ import Image from 'next/image';
 import Styles from '../styles/Curso.module.scss';
 
 const Curso = ({curso}) => {
-  const {nombre, resumen, imagen} = curso;
+  const {nombre, resumen, imagen, url} = curso;
   return (
       <div className={Styles.card}>
-        <Image 
-          width={120} 
-          height={70} 
-          layout="responsive" 
-          src={imagen} 
-          alt={nombre} 
-          priority 
-        />
-        <h3>{nombre}</h3>
-        <p>{resumen}</p>
+        <a href={url} target="_blank" without rel="noreferrer" className={Styles.enlaceCurso} >
+          <Image 
+            width={120} 
+            height={70} 
+            layout="responsive" 
+            src={imagen} 
+            alt={nombre} 
+            priority 
+          />
+          <h3>{nombre}</h3>
+          <p>{resumen}</p>
+        </a>
       </div>
   )
 }
